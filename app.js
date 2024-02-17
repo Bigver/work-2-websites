036fe059184f1781c8dfc41ce3db4bc0e03484ec
Bigver <0972356484a@gmail.com>
2024-02-17 13:06:52 +0700

    new changes

--- app.js
+++ app.js
@@ -1,31 +1,11 @@
-function getRandom(min, max) {
-    return Math.random() * (max - min) + min;
+const imgContent = document.querySelectorAll('.img-content-hover');
+
+function showImgContent(e) {
+  for(var i = 0; i < imgContent.length; i++) {
+    x = e.pageX;
+    y = e.pageY;
+    imgContent[i].style.transform = `translate3d(${x}px, ${y}px, 0)`;
   }
-  
-  var imgContainer = document.querySelector(".expose"),
-  imgSeq = document.querySelectorAll(".shuffle img"),
-  i = 1;
-  
-  imgContainer.classList.remove("expose");
-  imgContainer.classList.add("webanim");
-  
-  function racknstack() {
-    Array.prototype.forEach.call(imgSeq, function(photo) { 
-          setTimeout(function(){ 
-              photo.style.position = "absolute";
-              photo.style.width = getRandom(33,45)+"%";
-              photo.style.left = getRandom(-5,65)+"%";
-              photo.style.top = getRandom(-6,60)+"vh";
-              var animate = photo.animate([
-                  { opacity: '0', transform: 'rotate('+getRandom(-12,12)+'deg) scale(1.2)', boxShadow: '0 0 12px 12px rgba(0,0,0,.3)' },
-                  { opacity: '1', transform: 'rotate('+getRandom(-8,8)+'deg)', boxShadow: '0 0 6px 6px rgba(0,0,0,.3)' }
-                ], {
-          duration: 2000,
-          fill: 'forwards'
-        });                    
-      }, 1800*i)
-      i++;
-    })
-   }
-   
-  imagesLoaded(imgSeq, racknstack);
+};
+
+document.addEventListener('mousemove', showImgContent);
